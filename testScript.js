9476f12db2d7fcca377c8baef4870bc9201348ea
smcourtenay <smcourtenay@gmail.com>
2015-11-23 21:20:23 -0500

    Testing Out Push and Pull in GitHub

---
+++ testScript.js
@@ -0,0 +1,15 @@
+/*
+This is a commented out section (block commenting)
+
+Sam has created some general function calls, and Cole will complete them.
+
+*/
+
+
+
+
+
+
+
+
+testFunction(testVariable);
