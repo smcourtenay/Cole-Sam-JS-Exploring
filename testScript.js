39ff0cd6d08be2853166307e9b1d9d2264657699
ColeJSullivan <sullivan.colej@gmail.com>
2015-11-23 21:34:20 -0500

    Added Variable and function

--- testScript.js
+++ testScript.js
@@ -5,10 +5,12 @@ Sam has created some general function calls, and Cole will complete them.
 
 */
 
+var testVariable = "Test Words."
 
 
-
-
+function testFunction(SomeString){
+  console.log(SomeString);
+}
 
 
 
